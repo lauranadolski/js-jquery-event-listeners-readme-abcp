ba19586fd7fefbeb2035e03188c48ffcb3cf5d1c
Laura Nadolski <laura.nadolski@gmail.com>
2018-02-03 21:51:13 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -5,3 +5,40 @@ $(document).ready(function(){
 // call functions here
 
 });
+
+
+
+
+function getIt() {
+  $('p').on("click", function() {
+  alert("Hey!");
+  });
+}
+
+function frameIt() {
+  $('img').on('load', function() {
+   img.tasty;
+  });
+}
+
+function pressIt() {
+  
+}
+ 
+function submitIt() {
+  
+}
+
+
+
+
+
+
+
+$('h1').on("click", function() {
+  alert("You clicked me!");
+});
+
+$('img').on('load', function(){
+  console.log("I am a minion!");
+});
